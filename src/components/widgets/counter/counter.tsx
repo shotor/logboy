aeac031d85930be75bdf4f01197a15144304a090
shotor <shotor@shotor.com>
2020-04-13 14:33:19 +0200

    feat(counter): add chart

--- src/components/widgets/counter/counter.tsx
+++ src/components/widgets/counter/counter.tsx
@@ -1,6 +1,16 @@
 import React, { useContext, useState } from 'react'
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts'
 import { WidgetContext } from '../../../data/widgets'
-import { CounterModel } from './types'
+import { CounterModel, Value } from './types'
 import { Text } from '../../core/text'
 import { UpdateForm } from './update-form'
 import { WidgetContextAPI } from '../../../data/widgets/context'
@@ -11,6 +21,51 @@ type CounterProps = {
   id: number
 }
 
+const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+]
+
 export const Counter: React.FC<CounterProps> = ({ id }) => {
   const { read: resource, update } = useContext<CounterContextAPI>(
     WidgetContext
@@ -18,6 +73,20 @@ export const Counter: React.FC<CounterProps> = ({ id }) => {
   const [value, setValue] = useState(resource.read(id))
 
   const { title, createdOn, valueType, type, values } = value
+
+  // TODO: wont work for string vals
+
+  const numbers = (values as Value<number>[]).map((x) => x.value)
+
+  const min = numbers.reduce((acc, curr) => Math.min(acc, curr))
+  const max = numbers.reduce((acc, curr) => Math.max(acc, curr))
+  const avg = numbers.reduce((acc, curr) => acc + curr) / numbers.length
+
+  const minDiff = avg - min
+  const maxDiff = max - avg
+
+  const yMin = Math.floor(min - minDiff)
+  const yMax = Math.ceil(max + maxDiff)
 
   return (
     <>
@@ -47,14 +116,40 @@ export const Counter: React.FC<CounterProps> = ({ id }) => {
       />
       <hr />
       <Text variant="title" as="h2">
+        Track
+      </Text>
+      <ResponsiveContainer width="100%" height={300}>
+        <LineChart
+          data={values.map(({ date, value }) => ({
+            value,
+            date: date.toDateString(),
+          }))}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="date" padding={{ left: 30, right: 30 }} />
+          <YAxis domain={[yMin, yMax]} padding={{ top: 30, bottom: 30 }} />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="linear"
+            dataKey="value"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+      <hr />
+      <Text variant="title" as="h2">
         Manage
       </Text>
       <Text as="p">TODO</Text>
       <hr />
-      <Text variant="title" as="h2">
-        Track
-      </Text>
-      <Text as="p">TODO</Text>
     </>
   )
 }
